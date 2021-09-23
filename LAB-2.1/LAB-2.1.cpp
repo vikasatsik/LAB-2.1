f7a4e6e43b9d7e052c7334d9e485a96443b147f5
Igor <Igor@ACEDRAGOON>
2021-09-23 13:12:06 +0300

    Добавьте файлы проекта.

---
+++ LAB-2.1/LAB-2.1.cpp
@@ -0,0 +1,30 @@
+﻿// LAB-02.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// LAB_02.cpp
+// Сацик Вікторія
+// Лабораторна робота №2.1.
+// Лінійні програми.
+// Варіант 17
+
+#include <iostream>;
+#include <cmath>;
+
+using namespace std;
+
+int main()
+{
+    double m;
+    double z1;
+    double z2;
+
+    cout << "m = "; cin >> m;
+
+    z1 = (sqrt((pow((3 * m + 2), 2)) - 24 * m)) / (3 * sqrt(m) - 2 / sqrt(m));
+    z2 = sqrt(m);
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
